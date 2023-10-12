a40955b6bafc6b62f83ea78515c361575b3bf113
tora180405 <tora@gmail.com>
2023-10-12 14:24:20 +0700

    edit pemilihan2percobaan2-27.java

--- daspro_jobsheet6/Pemilihan2Percobaan2_27.java
+++ daspro_jobsheet6/Pemilihan2Percobaan2_27.java
@@ -16,8 +16,12 @@ public class Pemilihan2Percobaan2_27 {
         if(totalSudut == 180){
             if((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)){
                 System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+            }else if((sudut1 == 60) && (sudut2 == 60) && (sudut3 == 60)){
+                System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+            }else if((sudut1==sudut2) || (sudut1==sudut3) || (sudut3==sudut2)){
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
             }else{
-                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+                System.out.println("Segitiga tersebut adalah Segitiga sembarang");
             }
         }else{
             System.out.println("Bukan segitiga");
